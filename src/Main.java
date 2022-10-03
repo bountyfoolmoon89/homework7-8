44a2899c7928d73ecb2a2a198c0271d0dde836a6
Егор <bountyfoolmoon89@yandex.com>
2022-10-03 14:37:15 +0300

    7 tasks

--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,58 @@ public class Main {
         task3();
         System.out.println(" Task 4");
         task4();
+        System.out.println(" Task 5");
+        task5();
+        System.out.println(" Task 6");
+        task6();
+        System.out.println(" Task 7");
+        task7();
     }
-    public static void task1(){
+
+    public static void task1() {
         int i;
-        for (i = 1; i <= 10; i++){
+        for (i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
-    public static void task2(){
+
+    public static void task2() {
         int i;
-        for (i = 10; i > 0; i--){
+        for (i = 10; i > 0; i--) {
             System.out.println(i);
         }
     }
-    public static void task3(){
+
+    public static void task3() {
         int i;
-        for (i = 0; i < 17; i+=2){
+        for (i = 0; i < 17; i += 2) {
             System.out.println(i);
         }
     }
-    public static void task4(){
+
+    public static void task4() {
         int i;
-        for (i = 10; i >= -10; i--){
+        for (i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        int i;
+        for (i = 1904; i <= 2096; i += 4) {
+            System.out.println(i + " год является високосным");
+        }
+    }
+
+    public static void task6() {
+        int i;
+        for (i = 7; i <= 98; i += 7) {
+            System.out.println(i);
+        }
+    }
+    public static void task7(){
+        int i;
+        for (i = 1; i <= 512; i *= 2) {
             System.out.println(i);
         }
     }
